fb0bc94be4eeefbe2a32c6c7d9757b4830965f52
clarabernadou <clarabernadou56690@gmail.com>
2024-04-21 22:45:24 +0200

    [test] - create first tests for users + fixtures

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -1,8 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
+import { HttpClient } from '@angular/common/http';
 
 import { UserService } from './user.service';
+import { User, Users, UsersWithoutOne } from './user.fixtures';
 
 describe('UserService', () => {
   let service: UserService;
@@ -18,5 +20,29 @@ describe('UserService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get a user by id', () => {
+    service.getById = jest.fn().mockReturnValue(User);
+
+    const result = service.getById('1');
+
+    expect(result).toEqual(User);
+  });
+
+  it('should not get a user by id', () => {
+    service.getById = jest.fn().mockReturnValue(null);
+
+    const result = service.getById('1');
+
+    expect(result).toBeNull();
+  });
+
+  it('should delete a user', () => {
+    service.delete = jest.fn().mockReturnValue(null);
+
+    const result = service.delete('1');
+
+    expect(result).toEqual(null);
   });
 });
