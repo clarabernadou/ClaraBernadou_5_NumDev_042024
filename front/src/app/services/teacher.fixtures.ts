85b20449a91cac848ad10b3be5436d4e0ce809db
clarabernadou <clarabernadou56690@gmail.com>
2024-04-26 11:20:18 +0200

    [test] - add teacher tests + fixtures

---
+++ front/src/app/services/teacher.fixtures.ts
@@ -0,0 +1,31 @@
+export const Teacher = {
+    id: 1,
+    lastName: 'Toto',
+    firstName: 'Toto',
+    createdAt: '2024-04-18 23:30:34',
+    updatedAt: '2024-04-18 23:30:34',
+};
+
+export const Teachers = [
+    {
+        id: 1,
+        lastName: 'Toto',
+        firstName: 'Toto',
+        createdAt: '2024-04-18 23:30:34',
+        updatedAt: '2024-04-18 23:30:34',
+    },
+    {
+        id: 2,
+        lastName: 'Tata',
+        firstName: 'Tata',
+        createdAt: '2024-04-18 23:30:34',
+        updatedAt: '2024-04-18 23:30:34',
+    },
+    {
+        id: 3,
+        lastName: 'Tutu',
+        firstName: 'Tutu',
+        createdAt: '2024-04-18 23:30:34',
+        updatedAt: '2024-04-18 23:30:34',
+    }
+];
--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -1,22 +1,77 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { TeacherService } from './teacher.service';
+import { Teacher, Teachers } from './teacher.fixtures';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('TeacherService', () => {
   let service: TeacherService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports:[
-        HttpClientModule
-      ]
+        HttpClientModule, HttpClientTestingModule
+      ],
+      providers: [TeacherService]
     });
     service = TestBed.inject(TeacherService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return all teachers', () => {
+    service.all().subscribe(teachers => {
+      expect(teachers).toEqual(Teachers);
+    });
+
+    const req = httpMock.expectOne(`api/teacher`);
+    expect(req.request.method).toBe('GET');
+    req.flush(Teachers);
+  });
+
+  it('should not return all teachers', () => {
+    service.all().subscribe(
+      teachers => fail('error expected'),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(404);
+      }
+    );
+
+    const req = httpMock.expectOne(`api/teacher`);
+    expect(req.request.method).toBe('GET');
+    req.flush('Error', {status: 404, statusText: 'Not Found'});
+  });
+
+  it('should return a teacher', () => {
+    service.detail('1').subscribe(teacher => {
+      expect(teacher).toEqual(Teachers[0]);
+    });
+
+    const req = httpMock.expectOne(`api/teacher/1`);
+    expect(req.request.method).toBe('GET');
+    req.flush(Teachers);
+  });
+
+  it('should not return a teacher', () => {
+    service.detail('null').subscribe(
+      teacher => fail('error expected'),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(404);
+      }
+    );
+
+    const req = httpMock.expectOne(`api/teacher/null`);
+    expect(req.request.method).toBe('GET');
+    req.flush('Error', {status: 404, statusText: 'Not Found'});
   });
 });
