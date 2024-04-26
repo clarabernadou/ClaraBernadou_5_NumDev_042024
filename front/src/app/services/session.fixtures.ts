4d62547fd8fe55ccf1afdac0a02f06448698ae48
clarabernadou <clarabernadou56690@gmail.com>
2024-04-26 11:53:19 +0200

    [test] - add session tests + fixture

---
+++ front/src/app/services/session.fixtures.ts
@@ -0,0 +1,9 @@
+export const UserSessionInformation = {
+    token: '98a4b7e2-12c3-4d56-a789-0e1f2a3b4c5d',
+    type: 'string',
+    id: 1,
+    username: 'Toto',
+    firstName: 'Toto',
+    lastName: 'Toto',
+    admin: false,
+  }
--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,16 +2,81 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpErrorResponse } from '@angular/common/http';
+import { UserSessionInformation } from './session.fixtures';
 
 describe('SessionService', () => {
   let service: SessionService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers: [SessionService]
+    });
     service = TestBed.inject(SessionService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return logged status', () => {
+    service.$isLogged().subscribe(session => {
+      expect(session).toBeTruthy();
+    });
+  });
+
+  it('should return not logged status', () => {
+    service.$isLogged().subscribe(
+      session => fail('error expected'),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(404);
+      }
+    );
+  });
+
+  it('should log in', () => {
+    service.logIn(UserSessionInformation);
+    service.$isLogged().subscribe(session => {
+      expect(session).toBeTruthy();
+    });
+  });
+
+  it('should not log in', () => {
+    service.logIn(UserSessionInformation);
+    service.$isLogged().subscribe(
+      session => { 
+        expect(session).toBeFalsy(); 
+      },
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(404);
+      }
+    );
+  });
+
+  it('should log out', () => {
+    service.logOut();
+    service.$isLogged().subscribe(session => {
+      expect(session).toBeFalsy();
+    });
+  });
+
+  it('should not log out', () => {
+    service.logOut();
+    service.$isLogged().subscribe(
+      session => {
+        expect(session).toBeTruthy();
+      },
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(404);
+      }
+    );
   });
 });
