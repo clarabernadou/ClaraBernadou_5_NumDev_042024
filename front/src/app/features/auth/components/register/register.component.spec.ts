042e3b8fcd2adbe84a5a233cbd800c9434a75661
clarabernadou <clarabernadou56690@gmail.com>
2024-04-29 21:26:19 +0200

    [test] - add register tests

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -9,10 +9,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import { RegisterRequest } from '../../services/auth.service.fixtures';
+import { EmptyRegisterRequest } from './register.component.fixtures';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { throwError } from 'rxjs';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let router: Router;
+  let authService: AuthService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -32,9 +39,34 @@ describe('RegisterComponent', () => {
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
+    authService = TestBed.inject(AuthService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should submit with success', () => {
+    const registerRequest = RegisterRequest;
+    component.form.setValue(registerRequest);
+    component.submit();
+
+    expect(component.form.get('email')?.value).toEqual(registerRequest.email);
+    expect(component.form.get('firstName')?.value).toEqual(registerRequest.firstName);
+    expect(component.form.get('lastName')?.value).toEqual(registerRequest.lastName);
+    expect(component.form.get('password')?.value).toEqual(registerRequest.password);
+    expect(component.onError).toBeFalsy();
+  });
+
+  it('should handle register error', () => {
+    const resigterRequest = RegisterRequest;
+    const error = new Error('Register failed');
+    jest.spyOn(authService, 'register').mockReturnValue(throwError(error));
+
+    component.form.setValue(resigterRequest);
+    component.submit();
+
+    expect(component.onError).toBeTruthy();
   });
 });
