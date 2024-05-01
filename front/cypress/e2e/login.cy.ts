a592e145ab2e2e1c5abd361d14ae1ea734659e22
clarabernadou <clarabernadou56690@gmail.com>
2024-05-01 22:49:19 +0200

    [test] - add login tests

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -23,5 +23,65 @@ describe('Login spec', () => {
     cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
     cy.url().should('include', '/sessions')
-  })
+  });
+
+  it('Login not successfull because bad email', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        error: 'An error occurred',
+      },
+      statusCode: 400,
+    }).as('login');
+
+    cy.get('input[formControlName=email]').type("yogaa@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+    cy.wait('@login');
+
+    cy.url().should('not.include', '/sessions');
+
+    cy.contains('An error occurred').should('exist');
+  });
+
+  it('Login not successfull because bad password', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        error: 'An error occurred',
+      },
+      statusCode: 400,
+    }).as('login');
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!12344"}{enter}{enter}`)
+
+    cy.wait('@login');
+
+    cy.url().should('not.include', '/sessions');
+
+    cy.contains('An error occurred').should('exist');
+  });
+
+  it('Login not successfull because bad email and password', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        error: 'An error occurred',
+      },
+      statusCode: 400,
+    }).as('login');
+
+    cy.get('input[formControlName=email]').type("yogaa@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!12344"}{enter}{enter}`)
+
+    cy.wait('@login');
+
+    cy.url().should('not.include', '/sessions');
+
+    cy.contains('An error occurred').should('exist');
+  });
 });
