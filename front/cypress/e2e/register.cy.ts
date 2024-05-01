0ec7bcb5f181027d3ac9120ce4b14ef5929adbe1
clarabernadou <clarabernadou56690@gmail.com>
2024-05-01 22:49:36 +0200

    [test] - add register tests

---
+++ front/cypress/e2e/register.cy.ts
@@ -0,0 +1,47 @@
+describe('Register spec', () => {
+    it('Register successfull', () => {
+        cy.visit('/register')
+
+        cy.intercept('POST', '/api/auth/register', {
+            body: {
+            id: 1,
+            username: 'userName',
+            firstName: 'firstName',
+            lastName: 'lastName',
+            admin: true
+            },
+        })
+
+        cy.intercept(
+            {
+            method: 'GET',
+            url: '/api/session',
+            },
+            []).as('session')
+
+        cy.get('input[formControlName=firstName]').type("Toto")
+        cy.get('input[formControlName=lastName]').type("Yoga")
+        cy.get('input[formControlName=email]').type("yoga@studio.com")
+        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+        cy.url().should('include', '/login')
+    });
+
+    it('Register not successfull because email has already been used', () => {
+        cy.visit('/register')
+
+        cy.intercept('POST', '/api/auth/register', {
+            body: {
+              error: 'An error occurred',
+            },
+            statusCode: 409,
+        }).as('register');
+
+        cy.get('input[formControlName=firstName]').type("Toto")
+        cy.get('input[formControlName=lastName]').type("Yoga")
+        cy.get('input[formControlName=email]').type("yoga@studio.com")
+        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+        cy.url().should('not.include', '/login')
+    });
+  });
