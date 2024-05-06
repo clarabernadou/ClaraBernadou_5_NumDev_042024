4c348694f3081ac7c5582c144df94eff7f722949
clarabernadou <clarabernadou56690@gmail.com>
2024-05-06 22:22:59 +0200

    [test] - upgrade register tests + mock

--- front/cypress/e2e/pages/register.cy.ts
+++ front/cypress/e2e/pages/register.cy.ts
@@ -1,25 +1,41 @@
 describe('Register spec', () => {
-    it('Register page', () => {
+    it('Check register component', () => {
         cy.visit('/register')
 
+        cy.get('mat-card').should('exist')
+
+        cy.get('mat-card-header').should('exist')
         cy.get('.mat-card-title').contains('Register').should('exist')
+
+        cy.get('form').should('exist')
+        cy.get('mat-card-content').should('exist')
+        cy.get('mat-form-field').should('have.length', 4)
+
         cy.get('input[formControlName=firstName]').should('exist')
         cy.get('input[formControlName=lastName]').should('exist')
         cy.get('input[formControlName=email]').should('exist')
         cy.get('input[formControlName=password]').should('exist')
+
         cy.get('button').contains('Submit').should('exist')
+        cy.get('button[type="submit"]').should('be.disabled');
     });
 
     it('Register successfull', () => {
         cy.visit('/register')
 
+        const user = {
+            email: 'toto@test.com',
+            firstName: 'Toto',
+            lastName: 'Test',
+            admin: false
+        }
+
         cy.intercept('POST', '/api/auth/register', {
             body: {
-            id: 1,
-            username: 'userName',
-            firstName: 'firstName',
-            lastName: 'lastName',
-            admin: true
+                email: user.email,
+                firstName: user.firstName,
+                lastName:  user.lastName,
+                admin: user.admin
             },
         })
 
@@ -30,16 +46,26 @@ describe('Register spec', () => {
             },
             []).as('session')
 
-        cy.get('input[formControlName=firstName]').type("Toto")
-        cy.get('input[formControlName=lastName]').type("Yoga")
-        cy.get('input[formControlName=email]').type("yoga@studio.com")
-        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+        cy.get('input[formControlName=firstName]').type(user.firstName)
+        cy.get('input[formControlName=lastName]').type(user.lastName)
+        cy.get('input[formControlName=email]').type(user.email)
+        cy.get('input[formControlName=password]').type("test!1234")
+
+        cy.get('button[type="submit"]').should('not.be.disabled');
+        cy.get('span').contains('Submit').click()
 
         cy.url().should('include', '/login')
     });
 
     it('Register not successfull because email has already been used', () => {
         cy.visit('/register')
+
+        const user = {
+            email: 'toto@test.com',
+            firstName: 'Toto',
+            lastName: 'Test',
+            admin: false
+        }
 
         cy.intercept('POST', '/api/auth/register', {
             body: {
@@ -48,10 +74,15 @@ describe('Register spec', () => {
             statusCode: 409,
         }).as('register');
 
-        cy.get('input[formControlName=firstName]').type("Toto")
-        cy.get('input[formControlName=lastName]').type("Yoga")
-        cy.get('input[formControlName=email]').type("yoga@studio.com")
-        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+        cy.get('input[formControlName=firstName]').type(user.firstName)
+        cy.get('input[formControlName=lastName]').type(user.lastName)
+        cy.get('input[formControlName=email]').type(user.email)
+        cy.get('input[formControlName=password]').type("test!1234")
+
+        cy.get('button[type="submit"]').should('not.be.disabled');
+        cy.get('span').contains('Submit').click()
+
+        cy.get('span').contains('An error occurred').should('exist');
 
         cy.url().should('not.include', '/login')
     });
