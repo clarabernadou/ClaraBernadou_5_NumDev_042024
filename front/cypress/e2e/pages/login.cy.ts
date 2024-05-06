3e1331d4cd4c9e9bf890a096bcce75a5eec41385
clarabernadou <clarabernadou56690@gmail.com>
2024-05-06 22:22:14 +0200

    [test] - upgrade login tests

--- front/cypress/e2e/pages/login.cy.ts
+++ front/cypress/e2e/pages/login.cy.ts
@@ -1,96 +1,143 @@
 describe('Login spec', () => {
-  it('Login page', () => {
+    beforeEach(() => {
+      cy.intercept('GET', '/api/auth', { fixture: 'user.json' })
+    })
+
+  it('Check login component', () => {
     cy.visit('/login')
 
+    cy.get('mat-card').should('exist')
+
+    cy.get('mat-card-header').should('exist')
     cy.get('.mat-card-title').contains('Login').should('exist')
+
+    cy.get('form').should('exist')
+    cy.get('mat-card-content').should('exist')
+    cy.get('mat-form-field').should('have.length', 2)
+
     cy.get('input[formControlName=email]').should('exist')
     cy.get('input[formControlName=password]').should('exist')
+
+    cy.get('button[mat-icon-button]').contains('visibility_off').should('exist');
+    cy.get('button[mat-icon-button]').contains('visibility').click();
+    cy.get('button[mat-icon-button]').contains('visibility').should('exist');
+
     cy.get('button').contains('Submit').should('exist')
+    cy.get('button[type="submit"]').should('be.disabled');
+  });
+
+  it('Login not successful because bad email', () => {
+    cy.visit('/login');
+
+    const user = {
+      email: 'totoo@test.com',
+      password: 'test!1234',
+    };
+
+    cy.intercept('POST', '/api/auth/login', (req) => {
+      req.reply({
+        statusCode: 401,
+        body: { error: 'Bad credentials' },
+      });
+    });
+
+    cy.get('input[formControlName=email]').type(user.email);
+    cy.get('input[formControlName=password]').type(user.password);
+    cy.get('button').contains('Submit').click();
+
+    cy.fixture('user.json').then(users => {
+      const foundUser = users.find(u => u.email === user.email);
+      expect(foundUser).to.not.exist;
+    });
+
+    cy.url().should('not.include', '/sessions');
+
+    cy.get('p').contains('An error occurred').should('exist');
+  });
+
+  it('Login not successful because bad password', () => {
+    cy.visit('/login');
+
+    const user = {
+      email: 'toto@test.com',
+      password: 'test!12345',
+    };
+
+    cy.intercept('POST', '/api/auth/login', (req) => {
+      req.reply({
+        statusCode: 401,
+        body: { error: 'Bad credentials' },
+      });
+    });
+
+    cy.get('input[formControlName=email]').type(user.email);
+    cy.get('input[formControlName=password]').type(user.password);
+    cy.get('button').contains('Submit').click();
+
+    cy.fixture('user.json').then(users => {
+      const foundUser = users.find(u => u.email === user.email);
+      expect(foundUser).to.exist;
+      expect(foundUser.email).to.equal(user.email);
+    });
+
+    cy.url().should('not.include', '/sessions');
+
+    cy.get('p').contains('An error occurred').should('exist');
+  });
+
+  it('Login not successful because bad email and password', () => {
+    cy.visit('/login');
+
+    const user = {
+      email: 'totoo@test.com',
+      password: 'test!12345',
+    };
+
+    cy.intercept('POST', '/api/auth/login', (req) => {
+      req.reply({
+        statusCode: 401,
+        body: { error: 'Bad credentials' },
+      });
+    });
+
+    cy.get('input[formControlName=email]').type(user.email);
+    cy.get('input[formControlName=password]').type(user.password);
+    cy.get('button').contains('Submit').click();
+
+    cy.fixture('user.json').then(users => {
+      const foundUser = users.find(u => u.email === user.email);
+      expect(foundUser).to.not.exist;
+    });
+
+    cy.url().should('not.include', '/sessions');
+
+    cy.get('p').contains('An error occurred').should('exist');
   });
 
   it('Login successfull', () => {
     cy.visit('/login')
 
+    const user = {
+      email: 'toto@test.com',
+      password: 'test!1234',
+    }
+
     cy.intercept('POST', '/api/auth/login', {
       body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true
+        username: user.email,
+        password: user.password,
       },
-    })
+    });
 
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/api/session',
-      },
-      []).as('session')
+    cy.get('input[formControlName=email]').type(user.email)
+    cy.get('input[formControlName=password]').type(`${user.password}{enter}{enter}`)
 
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    cy.fixture('user.json').then(users => {
+      const foundUser = users.find(u => u.email === user.email);
+      expect(foundUser).to.exist;
+      expect(foundUser.email).to.equal(user.email);
+    });
 
     cy.url().should('include', '/sessions')
-  });
-
-  it('Login not successfull because bad email', () => {
-    cy.visit('/login')
-
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        error: 'An error occurred',
-      },
-      statusCode: 400,
-    }).as('login');
-
-    cy.get('input[formControlName=email]').type("yogaa@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
-
-    cy.wait('@login');
-
-    cy.url().should('not.include', '/sessions');
-
-    cy.contains('An error occurred').should('exist');
-  });
-
-  it('Login not successfull because bad password', () => {
-    cy.visit('/login')
-
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        error: 'An error occurred',
-      },
-      statusCode: 400,
-    }).as('login');
-
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!12344"}{enter}{enter}`)
-
-    cy.wait('@login');
-
-    cy.url().should('not.include', '/sessions');
-
-    cy.contains('An error occurred').should('exist');
-  });
-
-  it('Login not successfull because bad email and password', () => {
-    cy.visit('/login')
-
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        error: 'An error occurred',
-      },
-      statusCode: 400,
-    }).as('login');
-
-    cy.get('input[formControlName=email]').type("yogaa@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!12344"}{enter}{enter}`)
-
-    cy.wait('@login');
-
-    cy.url().should('not.include', '/sessions');
-
-    cy.contains('An error occurred').should('exist');
   });
 });
