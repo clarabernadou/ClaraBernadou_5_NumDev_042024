5ec7a8b439cc5931dab55306b24451248001ca6e
clarabernadou <clarabernadou56690@gmail.com>
2024-05-09 14:03:57 +0200

    [test] - add tests in app.cy.ts

--- front/cypress/e2e/app.cy.ts
+++ front/cypress/e2e/app.cy.ts
@@ -4,3 +4,5 @@ import './logout.cy';
 import './pages/login.cy';
 import './pages/register.cy';
 import './pages/create_session.cy';
+import './pages/update_session.cy';
+import './pages/session_list.cy';
