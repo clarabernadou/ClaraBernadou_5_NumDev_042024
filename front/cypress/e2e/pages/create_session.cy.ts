2409e906b42a5b8ffa45ca23536ad7ef2a108f48
clarabernadou <clarabernadou56690@gmail.com>
2024-05-09 14:01:52 +0200

    [test] - update create session test

--- front/cypress/e2e/pages/create_session.cy.ts
+++ front/cypress/e2e/pages/create_session.cy.ts
@@ -1,44 +1,80 @@
-describe('Account spec', () => {
+describe('Create session spec', () => {
     beforeEach(() => {
-        cy.intercept('GET', '/api/teacher', { fixture: 'teacher.json' })
+        cy.intercept('GET', '/api/teacher', { fixture: 'teachers.json' })
     })
 
     it('Login successfull', () => {
-      cy.visit('/login')
-  
-      cy.intercept('POST', '/api/auth/login', {
-        body: {
-          id: 1,
-          username: 'userName',
-          firstName: 'firstName',
-          lastName: 'lastName',
-          admin: true
-        },
-      })
-  
-      cy.intercept(
-        {
-          method: 'GET',
-          url: '/api/session',
-        },
-        []).as('session')
-  
-      cy.get('input[formControlName=email]').type("yoga@studio.com")
-      cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
-  
-      cy.url().should('include', '/sessions')
+        cy.visit('/login')
+
+        const user = {
+          email: 'toto@test.com',
+          password: 'test!1234',
+          admin: true,
+        }
+
+        cy.intercept('POST', '/api/auth/login', {
+          body: {
+            username: user.email,
+            password: user.password,
+            admin: user.admin,
+          },
+        });
+
+        cy.get('input[formControlName=email]').type("yoga@studio.com")
+        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+        cy.fixture('users.json').then(users => {
+          const foundUser = users.find(u => u.email === user.email);
+          expect(foundUser).to.exist;
+          expect(foundUser.email).to.equal(user.email);
+        });
+
+        cy.url().should('include', '/sessions')
+
+        cy.get('span').contains('Create').click()
     });
 
-    it('Create a session', () => {
+    it('Check create session page', () => {
+        cy.url().should('include', '/sessions/create')
+
+        cy.get('mat-card').should('exist')
+
+        cy.get('mat-card-title').should('exist')
+        cy.get('h1').contains('Create session').should('exist')
+
+        cy.get('button').contains('arrow_back').should('exist')
+        cy.get('button').contains('arrow_back').click()
+        cy.url().should('include', '/sessions')
+
+        // Return to the create session page
         cy.get('span').contains('Create').click()
 
+        cy.get('form').should('exist')
+        cy.get('mat-form-field').should('have.length', 4)
+
+        cy.get('input[formControlName=name]').should('exist')
+        cy.get('input[formControlName=date]').should('exist')
+        cy.get('mat-select[formControlName="teacher_id"]').should('exist')
+        cy.get('mat-select[formControlName="teacher_id"]').click()
+        cy.get('mat-option').should('have.length', 2);
+        cy.get('mat-option').contains('Margot DELAHAYE').should('exist')
+        cy.get('mat-option').contains('Hélène THIERCELIN').should('exist')
+        cy.get('textarea[formControlName=description]').should('exist')
+
+        cy.get('span').contains('Save').should('exist')
+        cy.get('button[type="submit"]').should('be.disabled');
+
+        cy.get('body').click()
+    })
+
+    it('Create session', () => {
         cy.get('input[formControlName=name]').type('Yoga')
         cy.get('input[formControlName=date]').type('2024-05-06')
         cy.get('mat-select[formControlName="teacher_id"]').click();
-        cy.get('mat-option').should('have.length', 2);
         cy.get('mat-option').contains('Margot DELAHAYE').click()
         cy.get('textarea[formControlName=description]').type('A yoga session')
 
+        cy.get('button[type="submit"]').should('not.be.disabled');
         cy.get('span').contains('Save').click()
         cy.url().should('include', '/sessions')
     })
