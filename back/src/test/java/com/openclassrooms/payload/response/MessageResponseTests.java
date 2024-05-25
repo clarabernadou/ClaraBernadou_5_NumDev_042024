959d8a3daecef66baadc6accc7e31dd1d9ee8183
clarabernadou <clarabernadou56690@gmail.com>
2024-05-25 16:51:54 +0200

    [test] - response unit tests

---
+++ back/src/test/java/com/openclassrooms/payload/response/MessageResponseTests.java
@@ -0,0 +1,34 @@
+package com.openclassrooms.payload.response;
+
+import com.openclassrooms.starterjwt.payload.response.MessageResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class MessageResponseTests {
+
+    @Mock
+    private MessageResponse messageResponse;
+
+    private String message;
+
+
+    @BeforeEach
+    void setUp() {
+        message = "This is a message";
+
+        messageResponse = new MessageResponse(message);
+    }
+
+    @Test
+    void shouldSetAndGetMessage() {
+        messageResponse.setMessage(message);
+
+        assertEquals(message, messageResponse.getMessage());
+    }
+}
