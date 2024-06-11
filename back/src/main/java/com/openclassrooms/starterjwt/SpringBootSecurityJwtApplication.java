0d20dc3b7d835b6c420e652652433474b65c3747
clarabernadou <clarabernadou56690@gmail.com>
2024-06-11 18:34:49 +0200

    [misc] - add dotenv in project (importation/config/integration) + .env file + update application.properties with new keys

--- back/src/main/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplication.java
+++ back/src/main/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class SpringBootSecurityJwtApplication {
 	public static void main(String[] args) {
+	Dotenv dotenv = Dotenv.load();
+	System.setProperty("DB_URL", dotenv.get("DB_URL"));
+	System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+	System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
     SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
 	}
 }
